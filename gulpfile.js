9cfb8aa6920f44a14bc35bffdc3d99accbfd3a25
Madguy <madguy@drumcan.in>
2018-12-11 09:49:25 +0900

    gulp4に対応

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,24 @@ const plumber = require('gulp-plumber');
 const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 const named = require('vinyl-named');
-const sequence = require('run-sequence');
 const del = require('del');
 const config = require('./gulp.config');
 
 const env = {
 	get mode() {
 		return /^prod/.test(process.env.NODE_ENV) ? 'production' : 'development';
-	}
+	},
 };
 
-gulp.task('clean:js', (next) => {
-	del([`${config.paths.js.dist}/**/*.min.js`, `${config.paths.js.dist}/**/*.map`], next);
+gulp.task('clean:js', () => {
+	return del([`${config.paths.js.dist}/**/*.min.js`, `${config.paths.js.dist}/**/*.map`]);
 });
 
-gulp.task('clean:css', (next) => {
-	del([`${config.paths.css.dist}/**/*.css`, `${config.paths.css.dist}/**/*.map`], next);
+gulp.task('clean:css', () => {
+	return del([`${config.paths.css.dist}/**/*.css`, `${config.paths.css.dist}/**/*.map`]);
 });
 
-gulp.task('clean', ['clean:js', 'clean:css']);
+gulp.task('clean', gulp.parallel('clean:js', 'clean:css'));
 
 gulp.task('compile:js', () => {
 	return gulp.src([`${config.paths.js.src}/*.js`, `!${config.paths.js.src}/**/*.min.js`])
@@ -42,18 +41,17 @@ gulp.task('compile:css', () => {
 		.pipe(gulp.dest(config.paths.css.dist));
 });
 
-gulp.task('build', (next) => {
-	sequence(['compile:js', 'compile:css'], next);
-});
+gulp.task('build', gulp.series('compile:js', 'compile:css'));
 
-gulp.task('watch:js', ['compile:js'], () => {
-	gulp.watch([`${config.paths.js.src}/**/*.js`, `!${config.paths.js.src}/**/*.min.js`], ['compile:js']);
-});
+gulp.task('watch:js', gulp.series('compile:js', () => {
+	gulp.watch([`${config.paths.js.src}/**/*.js`, `!${config.paths.js.src}/**/*.min.js`], gulp.task('compile:js'));
+}));
 
-gulp.task('watch:css', ['compile:css'], () => {
-	gulp.watch(`${config.paths.css.src}/**/*.{scss, sass}`, ['compile:css']);
-});
+gulp.task('watch:css', gulp.series('compile:css', () => {
+	gulp.watch(`${config.paths.css.src}/**/*.{scss, sass}`, gulp.task('compile:css'));
+}));
 
-gulp.task('watch', (next) => {
-	sequence('build', ['watch:js', 'watch:css'], next);
-});
+gulp.task('watch', gulp.series('build', () => {
+	gulp.watch([`${config.paths.js.src}/**/*.js`, `!${config.paths.js.src}/**/*.min.js`], gulp.task('compile:js'));
+	gulp.watch(`${config.paths.css.src}/**/*.{scss, sass}`, gulp.task('compile:css'));
+}));
